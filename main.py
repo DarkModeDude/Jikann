c4526657bff3c4b0c71b496e0c3d8ef73f5fef30
Dark <242133138+DarkModeDude@users.noreply.github.com>
2026-07-03 22:50:35 +0900

    set up simple base

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 # this is the main file where all the main logic is run
 
-def main():
-    pass # replace with code later
+from datetime import *
+# import json
 
+
+StartTime = datetime.now(timezone.utc)
+
+def main():
+    while True:
+        prompt = input("prompt: ")
+
+        if prompt == "start":
+            StartTime = datetime.now(timezone.utc)
+            print("Started Timer At: " + str(StartTime))
+        elif prompt == "stop":
+            print("Stoped timer, you spent: " + str(datetime.now(timezone.utc) - StartTime) + "on that")
+        elif prompt == "quit":
+            break
 
 if __name__ == "__main__":
     main()
