38c1af2aa7648181739a2b0a43d737ce0c3eadc8
Dark <242133138+DarkModeDude@users.noreply.github.com>
2026-07-24 23:12:54 +0900

    add comments

--- main.py
+++ main.py
@@ -21,21 +21,27 @@ def CreateBoard(BoardName=""):
 
 
 def main():
+    
+    # multi prompt loop variables:
     StartTime = datetime.now(timezone.utc)
     timing = False
     Currentboard = "NoBoard.json"
 
+    # prompt loop:
     while True:
-        prompt = input(f"{Currentboard.split(".")[0]}: ")
+        prompt = input(f"{Currentboard.split(".")[0]}: ") # get the current command
 
+        # starting a timer:
         if prompt == "start":
             if (timing == False):
                 StartTime = datetime.now(timezone.utc)
                 timing = True
                 print("Started Timer At: " + str(StartTime))
             else:
-                print("you where already timing somthing, you must stop it to start another") # we should make it so that you can run multible at once somt time in the future
+                print("you where already timing somthing, you must stop it to start another")
+                # we should make it so that you can run multible at once somt time in the future
 
+        # stoping a timer:
         elif prompt == "stop":
             if timing:
                 print("Stoped timer, you spent: " + str(datetime.now(timezone.utc) - StartTime) + "on that")
@@ -44,6 +50,7 @@ def main():
             else:
                 print("you where not timing anything")
         
+        # createing a new time board:
         elif prompt == "new board" or prompt == "new":
             NewBoardName = GetBoardName("what should the board be called? ")
             if DoesBoardExist(NewBoardName):
@@ -53,6 +60,7 @@ def main():
                 CreateBoard(NewBoardName)
                 Currentboard = NewBoardName
         
+        # loading a differnt board:
         elif prompt == "load board" or prompt == "load":
             LoadingBoard = GetBoardName("what is the new baord name? ")
             if DoesBoardExist(LoadingBoard):
@@ -61,6 +69,7 @@ def main():
             else:
                 print("that board does not exist in the current directory")
 
+        # quiting the program:
         elif prompt == "quit":
             break
 
