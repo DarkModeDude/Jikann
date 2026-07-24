d5f1ac1c52930b254d8a4f6a0335418daed07615
Dark <242133138+DarkModeDude@users.noreply.github.com>
2026-07-24 18:16:02 +0900

    made it not start and stop over and over again

--- main.py
+++ main.py
@@ -4,17 +4,26 @@ from datetime import *
 # import json
 
 
-StartTime = datetime.now(timezone.utc)
-
 def main():
+    StartTime = datetime.now(timezone.utc) # i hate python
+    timing = False
     while True:
         prompt = input("prompt: ")
 
         if prompt == "start":
-            StartTime = datetime.now(timezone.utc)
-            print("Started Timer At: " + str(StartTime))
+            if (timing == False):
+                StartTime = datetime.now(timezone.utc)
+                timing = True
+                print("Started Timer At: " + str(StartTime))
+            else:
+                print("you where already timing somthing, you must stop it to start another") # we should make it so that you can run multible at once somt time in the future
         elif prompt == "stop":
-            print("Stoped timer, you spent: " + str(datetime.now(timezone.utc) - StartTime) + "on that")
+            if timing:
+                print("Stoped timer, you spent: " + str(datetime.now(timezone.utc) - StartTime) + "on that")
+                timing = False
+                # save the time chunk to memory or somthing
+            else:
+                print("you where not timing anything")
         elif prompt == "quit":
             break
 
